277d92aabb5c158022b5b5b900c06c0e970cf0bf
wangkodok <wangkodok@gmail.com>
2022-02-13 23:20:35 +0900

    메인 페이지: 비주얼 텍스트 박스 텍스트 리스트 클릭 작업

--- js/main.js
+++ js/main.js
@@ -2,11 +2,43 @@ window.addEventListener('load', () => {
   visual();
 });
 
-// 비주얼 텍스트
+// 비주얼 텍스트 박스
 function visual() {
   const visualTitle = document.querySelector('.visual .text-box');
   visualTitle.classList.add('on');
-}
+
+  // 텍스트 리스트
+  function visualTitleList() {
+    const listText = document.querySelectorAll('.visual .text-list .txt');
+
+    for (let i = 0; i < listText.length; i++) {
+      listText[i].addEventListener('click', (e) => {
+        textListLnb(e);
+      });
+
+      // 클래스 on 추가/제거
+      function textListLnb(e) {
+        let lnbClassName = e.target.parentNode.querySelector('.text-list-lnb');
+        if (lnbClassName.className === 'text-list-lnb') {
+          classNameRemove();
+          lnbClassName.classList.add('on');
+        } else {
+          lnbClassName.classList.remove('on');
+        }
+      }
+
+      // 클래스 on 전체 제거
+      function classNameRemove() {
+        for (let i = 0; i < listText.length; i++) {
+          let target = listText[i].nextSibling.nextSibling;
+          target.classList.remove('on');
+        }
+      }
+    }
+  }
+
+  visualTitleList();
+};
 
 // 스크롤시 헤더 스타일 변경
 function headerScroll() {
